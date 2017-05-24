a87a8f13e5ab608e9efcc72959bcf02f42ee3b36
mgp <mgp@ebidding.com.cn>
2017-05-24 16:32:59 +0800

    升级 spring4 log4j2

--- dubbo-container/dubbo-container-spring/src/main/java/com/alibaba/dubbo/container/spring/SpringContainer.java
+++ dubbo-container/dubbo-container-spring/src/main/java/com/alibaba/dubbo/container/spring/SpringContainer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2011 Alibaba Group.
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,17 +15,15 @@
  */
 package com.alibaba.dubbo.container.spring;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.alibaba.dubbo.common.extension.ExtensionLoader;
 import com.alibaba.dubbo.common.logger.Logger;
 import com.alibaba.dubbo.common.logger.LoggerFactory;
 import com.alibaba.dubbo.common.utils.ConfigUtils;
 import com.alibaba.dubbo.container.Container;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * SpringContainer. (SPI, Singleton, ThreadSafe)
- * 
+ *
  * @author william.liangf
  */
 public class SpringContainer implements Container {
@@ -33,11 +31,11 @@ public class SpringContainer implements Container {
     private static final Logger logger = LoggerFactory.getLogger(SpringContainer.class);
 
     public static final String SPRING_CONFIG = "dubbo.spring.config";
-    
+
     public static final String DEFAULT_SPRING_CONFIG = "classpath*:META-INF/spring/*.xml";
 
     static ClassPathXmlApplicationContext context;
-    
+
     public static ClassPathXmlApplicationContext getContext() {
 		return context;
 	}
